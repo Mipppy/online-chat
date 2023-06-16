2f207b7ed792222864ab71a48d835dc1066407d1
Mipppy <122882588+Mipppy@users.noreply.github.com>
2023-06-16 07:23:54 -0400

    Updated ChatServer

---
+++ chatServer/client/client.js
@@ -0,0 +1,36 @@
+      //vars
+      const socket = io();
+      const form = $('#chat-form');
+      const input = $('#chat-input');
+      const messages = $('#messages');
+
+      d = prompt("Enter a username for this session...") //Getting username asap and emitting fake chat message to show connect/disconnect
+
+      if (d == null) {
+        location.reload(true)
+      }
+      socket.emit('chat message', d + " connected!") 
+
+      form.submit((e) => {
+        e.preventDefault();
+        if (input.val()) { //Sumbitting message
+          socket.emit('chat message', d + ': ' + input.val());
+          input.val('');
+        }
+      });
+
+      socket.on('chat message', (msg) => {
+        if (msg == "null connected!" || msg =="null disconnected!") {
+          console.log("null cannot be entered!") //more null errors
+        }
+        else if(d == null) {
+          location.reload(true)
+        }
+        else {
+        messages.append($('<li>').text(msg)); //adding message
+        }
+       });
+    
+      socket.on('disconnectmsg', (space) => {
+        socket.emit('chat message', d + ' disconnected!') //Same deal without username part
+      })
---
+++ chatServer/server.js
@@ -0,0 +1,51 @@
+
+// const express = require('express');
+// const app = express();
+// const http = require('http').createServer(app);
+// const io = require('socket.io')(http);
+
+// app.use(express.static(__dirname + '/html'));
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   io.emit('connectmsg', " ")
+
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//     io.emit('disconnectmsg', " ")
+//   });
+//   socket.on('chat message', (msg) => {
+//     console.log('message: ' + msg);
+//     io.emit('chat message', msg);
+//   });
+// });
+
+// http.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
+
+
+
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/client/'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    io.emit('disconnectmsg', " ")
+  });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
